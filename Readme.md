dcdeec0073c750baae188e8f23bbdf8874d8440a
xianju6x <xianjunx.zheng@intel.com>
2021-12-01 14:26:55 +0800

    init commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+$./nonvirtual
+```
+A construct
+B construct
+A destruct
+--------------
+A construct
+B construct
+B destruct
+A destruct
+```
+
+$./virtual
+
+```
+A construct
+B construct
+B destruct
+A destruct
+--------------
+A construct
+B construct
+B destruct
+A destruct
+```
---
+++ nonvirtual_destructor.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class A{
+	public:
+		A(){
+			std::cout<<"A construct\n";
+			ptr = new char[100];
+		}
+		~A(){
+			delete ptr;
+			std::cout<<"A destruct\n";
+		}
+	private:
+		char *ptr;
+};
+ 
+class B:public A{
+	public:
+		B(){
+			std::cout<<"B construct\n";
+			name = new char[100];
+		}
+		~B(){
+			delete name;
+			std::cout<<"B destruct\n";
+		}
+	private:
+		char *name;
+};
+
+int main(int argc, const char *argv[])
+{
+	A *a = new B();
+	delete a;
+	std::cout<<"--------------\n";
+	B *b = new B();
+	delete b;
+	return 0;
+}
